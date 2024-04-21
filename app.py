e57a7a8cb9ba14963877519f389fc1596a4a2e8c
Faizan Patel <faizan2786@hotmail.com>
2024-04-21 18:27:40 +0100

    Data loaded from Postgres sql db

--- app.py
+++ app.py
@@ -1,10 +1,62 @@
 from flask import Flask, jsonify
+import psycopg2 # postgres SQL api
+import os
 
-# in-memory database (in json format) to mimic db data
-database = {
-    1 : {"id": 1, "name": "Faizan Patel", "Age": "30" },
-    2 : {"id": 2, "name": "John Doe", "Age": "42" }
-}
+from flask.json.provider import DefaultJSONProvider
+import datetime
+
+# Database configuration
+db_host = "localhost"
+db_port = "5432"  # Default PostgreSQL port
+db_name = "webapp_db"
+db_user = os.getenv("DB_USER")
+db_password = os.getenv("DB_PASSWORD")
+
+db_table_name = "users"
+
+if None in (db_user, db_password):
+    raise RuntimeError("DB user and/or password not set by environment variables.")
+
+def get_db_connection():
+    # Connect to PostgreSQL
+    connection = psycopg2.connect(
+        host=db_host,
+        port=db_port,
+        dbname=db_name,
+        user=db_user,
+        password = db_password
+    )
+
+    return connection
+
+# get user by its id from the Postgres database
+def get_user_by_id(id: int, skip_nulls=True):
+
+    # Query data from PostgreSQL
+    cursor = get_db_connection().cursor()
+    cursor.execute(f'SELECT * FROM {db_table_name} WHERE id = {id};')
+    result = cursor.fetchone()
+
+    if not result:
+        return None
+
+    user = {} # prepare user object with their column labels
+    for col, val in zip(cursor.description, result):
+        if skip_nulls and val is None: # skip the null values if the option is set
+            continue
+        user[col[0]] = val
+
+    cursor.close()
+    return user
+
+# define custom json encoder 
+# (this is required to prevent the jsonify from automatically
+# converting the date objects to a datetime strings)
+class CustomJSONEncoder(DefaultJSONProvider):
+    def default(self, obj):
+        if isinstance(obj, datetime.date):
+            return obj.strftime('%Y-%m-%d')  # Format date as string
+        return super().default(obj)
 
 class Errors():
     @staticmethod
@@ -13,14 +65,15 @@ class Errors():
 
 app = Flask(__name__) # take the name of the app from the module name i.e. app.py
 
-@app.route("/user/<int:id>")
+@app.route('/user/<int:id>', methods=['GET'])
 def get_user(id: int):
-    user = database.get(id, None)
+    user = get_user_by_id(id)
     if user:
-        return jsonify(user)
+        return jsonify(user), 200
     else:
-        return jsonify(Errors.user_not_found(id))
+        return jsonify(Errors.user_not_found(id)), 404
     
 # run the app
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.json = CustomJSONEncoder(app) # use custom json encode for handling dates correctly
+    app.run(debug=False)
